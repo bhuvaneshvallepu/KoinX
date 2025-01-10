795120bae0505862567bfc89d3597170b988fed0
bhuvanesh <bhuvaneshvallepu@gmail.com>
2025-01-11 03:30:48 +0530

    first commit

--- Routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getStats } = require('../controllers/controller.js');
-
-const router = express.Router();
-
-router.get('/stats', getStats);
-
-module.exports = router;
